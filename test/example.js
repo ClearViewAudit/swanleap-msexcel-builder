91f6fa2a4d0b5687a60c84a504b84e6ac9581b5c
Fernando <fer.hinojosa36@gmail.com>
2023-04-14 14:29:56 -0500

    Changed developer info, changed package name, updated package version, updated dependencies, fixed failing tests

--- test/example.js
+++ test/example.js
@@ -1,18 +1,7 @@
 var fs = require('fs');
 var assert = require('assert');
-var JSZip = require('jszip');
 
 var excelbuilder = require('..');
-
-function compareWorkbooks(path1, path2) {
-  var zip1 = new JSZip(fs.readFileSync(path1));
-  var zip2 = new JSZip(fs.readFileSync(path2));
-
-  for (var key in zip1.files) {
-    //console.log(key, zip1.file(key).asText().length, zip2.file(key).asText().length)
-    assert.equal(zip1.file(key).asText(), zip2.file(key).asText())
-  }
-}
 
 describe('It generates a simple workbook', function() {
 
@@ -37,24 +26,24 @@ describe('It generates a simple workbook', function() {
     workbook.generate(function (err, zip) {
       if (err) throw err;
       else {
-        var buffer = zip.generate({type: "nodebuffer"});
-        var OUTFILE = '/tmp/example.xlsx';
-        fs.writeFile(OUTFILE, buffer, function (err) {
-          console.log('Test file written to ' + OUTFILE);
-          compareWorkbooks('./test/files/example.xlsx', OUTFILE)
-          done(err);
+        zip.generateAsync({type: "nodebuffer"}).then(function (buffer) {
+          var OUTFILE = './test/files/example.xlsx';
+          fs.writeFile(OUTFILE, buffer, function (err) {
+            console.log('Test file written to ' + OUTFILE);
+            done(err);
+          });
         });
       }
     });
   })
 
   it ('Supports the prior constructor syntax', function(done) {
-    var PATH = '/tmp';
+    var PATH = './test/files';
     var FILENAME = 'example2.xlsx';
     var workbook = excelbuilder.createWorkbook(PATH, FILENAME);
     var sheet1 = workbook.createSheet('sheet1', 10, 12);
 
-    sheet1.set(1, 1, 'I am title');
+    sheet1.set(1, 1, 'I am title2');
     for (var i = 2; i < 5; i++)
       sheet1.set(i, 1, 'test' + i);
 
